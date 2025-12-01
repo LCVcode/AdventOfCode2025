4579c3891b6287d5e2b84b64ebc7fdf86b251327
Connor Chamberlain <connor.chamberlain@canonical.com>
2025-11-30 22:00:38 -0700

    Initial commit
    * Includes a utils function to load inputs

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+
+import "fmt"
+
+
+func main() {
+	fmt.Println("It's finally time for Advent of Code!")
+}
---
+++ utils.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+)
+
+func ReadFileLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %s: %w", filePath, err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file %s: %w", filePath, err)
+	}
+	return lines, nil
+}
