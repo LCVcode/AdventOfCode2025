4854d4f8f42b96dcf2d01ebc3d29081504847d39
Connor Chamberlain <connor.chamberlain@canonical.com>
2025-12-03 01:56:47 -0700

    Day 2 part 2 done

--- day02/gift-shop.go
+++ day02/gift-shop.go
@@ -16,11 +16,24 @@ func splitRange(rng string) (string, string, error) {
 	return rng[:i], rng[i+1:], nil
 }
 
+func nDupeInt(sub, n int) int {
+	str := strconv.Itoa(sub)
+	result, _ := strconv.Atoi(strings.Repeat(str, n))
+	return result
+}
+
 func dupeInt(sub int) int {
 	// Duplicates an integer (9 -> 99 or 101 -> 101101)
-	str := strconv.Itoa(sub)
-	result, _ := strconv.Atoi(strings.Repeat(str, 2))
-	return result
+	return nDupeInt(sub, 2)
+}
+
+func Factorize(n int) []int {
+	var facts []int
+	for i := 1; i <= n/2; i++ {
+		if n%i != 0 { continue }
+		facts = append(facts, i)
+	}
+	return facts
 }
 
 func Part1() int {
@@ -48,6 +61,7 @@ func Part1() int {
 		if len(hi) % 2 == 1 {
 			hi = strings.Repeat("9", len(hi) - 1)
 		}
+		// Shortcut out if range is now undefined
 		if len(lo) > len(hi) { continue }
 
 		// Limit strings are each split in half
@@ -84,6 +98,72 @@ func Part1() int {
 	return total
 }
 
+
+// chunk splits s into substrings of length n.
+// The last chunk may be shorter.
+func chunk(s string, n int) []string {
+    if n <= 0 || s == "" {
+        return nil
+    }
+    out := make([]string, 0, (len(s)+n-1)/n)
+    for len(s) > 0 {
+        if len(s) < n {
+            out = append(out, s)
+            break
+        }
+        out = append(out, s[:n])
+        s = s[n:]
+    }
+    return out
+}
+
+
+func doesNCycle(str string) bool {
+	facts := Factorize(len(str))
+	for _, fact := range facts {
+		chunks := chunk(str, fact)
+		allSame := true
+		for _, c := range chunks[1:] {
+			if c != chunks[0] {
+				allSame = false
+				break
+			}
+		}
+		if allSame { return true }
+	}
+	return false
+}
+
+
+func BruteForceNCycles(lo, hi string) int {
+	var total int
+	ilo, _ := strconv.Atoi(lo)
+	ihi, _ := strconv.Atoi(hi)
+	for i := ilo; i <= ihi; i++ {
+		str := strconv.Itoa(i)
+		if doesNCycle(str) {
+			total += i
+		}
+
+	}
+	return total
+}
+
+
 func Part2() int {
-	return 0
+	raw, err := utils.ReadFileLines("day02/inputs.txt")
+	if err != nil {
+		panic(err)
+	}
+	var total int
+	ranges := strings.Split(raw[0], ",")
+
+	for _, rng := range ranges {
+		if len(rng) == 0 { continue }
+		lo, hi, err := splitRange(rng)
+		if err != nil { panic(err) }
+		total += BruteForceNCycles(lo, hi)
+	}
+
+	return total
 }
